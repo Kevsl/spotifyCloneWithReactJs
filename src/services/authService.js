f70258a6d11dbd50da9865fb15b88d757ece0a03
Kevin Terrier <kevin.terrier@le1817.com>
2022-07-20 16:37:00 +0200

    OK

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import qs from "qs";
 
 export const getAuth = async () => {
-  const clientId = "a62ba728282644148852438decf6601e";
-  const clientSecret = "a42ba0cd936e419fb08c7cf9d7bc1c0e";
+  const clientId = "";
+  const clientSecret = "";
 
   const headers = {
     headers: {
